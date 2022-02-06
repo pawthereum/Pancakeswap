adff84bb7c82d1ebece23319f8f28ca70b7e779a
Myk <89474773+mykcryptodev@users.noreply.github.com>
2022-02-05 21:12:02 -0500

    fix some broken images

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -46,8 +46,8 @@ const BodyWrapper = styled.div`
   }
 
   ${({ theme }) => theme.mediaQueries.lg} {
-    background-image: url('/images/arch-${({ theme }) => (theme.isDark ? 'dark' : 'light')}.svg'),
-      url('/images/left-pancake.svg'), url('/images/right-pancake.svg');
+    background-image: url('/Pancakeswap/images/arch-${({ theme }) => (theme.isDark ? 'dark' : 'light')}.svg'),
+      url('/Pancakeswap/images/left-pancake.svg'), url('/Pancakeswap/images/right-pancake.svg');
     background-repeat: no-repeat;
     background-position: center 420px, 10% 230px, 90% 230px;
     background-size: contain, 266px, 266px;
