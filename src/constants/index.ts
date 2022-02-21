1f86238ef9791d80ab35faa4a94eead222686f68
Myk <89474773+mykcryptodev@users.noreply.github.com>
2022-02-21 13:20:03 -0500

    update pawth

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -17,7 +17,7 @@ export const BUSD = new Token(swapChainId, '0x78867BbEeF44f2326bF8DDd1941a443938
 export const USDT = new Token(swapChainId, '0x7ef95a0fee0dd31b22626fa2e10ee6a223f8a684', 18, 'USDT', 'Tether USD');
 export const ETH = new Token(swapChainId, '0x8babbb98678facc7342735486c851abd7a0d17ca', 18, 'ETH', 'Ethereum');
 export const WBNB = new Token(swapChainId, '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c', 18, 'WBNB', 'Wrapped BNB');
-export const PAWTH = new Token(swapChainId, '0x5abd80b8108f90c8525a183547d6ecc004112c22', 9, 'PAWTH', 'Pawthereum')
+export const PAWTH = new Token(swapChainId, '0x409e215738E31d8aB252016369c2dd9c2008Fee0', 9, 'PAWTH', 'Pawthereum')
 
 const WETH_ONLY: ChainTokenList = {
   [ChainId.MAINNET]: [WETH[ChainId.MAINNET]],
